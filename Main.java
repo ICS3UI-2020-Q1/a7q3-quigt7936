35bb64872cfb7820e173def4e040f312c065e172
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-13 14:02:18 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Creates random amounts of astrics a given amount of times
+ * @author Thomas Quigley
  */
 public class Main {
-
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+  public static void chaos(int numbLines) {
+    // makes numbLines amount of lines
+    for (int i = 0; i < numbLines; i++) {
+      // create the random
+      int randNumb = (int)(Math.random()*(5 - 1 + 1)) + 1;
+     
+      // create a random amount of astrics
+      for (int j = 0; j < randNumb - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println("*");
+    }
+    
+      
+  }
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    //ask for the users number
+    System.out.println("Please enter a number.");
+    int numbLines = input.nextInt();
+    // perform chaos
+    chaos(numbLines);
     
   }
 }
